15774fe40cafad1f2250e7d7f84690cfa86fd373
kmsheng <kmsh3ng@gmail.com>
2016-05-17 20:50:30 +0800

    add header

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
+import {MenuButton} from '../../components';
 
 export default class App extends Component {
 
   render() {
     return (
-      <div className="home">Hello World</div>
+      <div className="home">
+        <div className="jumbotron"></div>
+        <header>
+          <div className="header">
+            <h1>Dharma Treasure</h1>
+            <MenuButton />
+          </div>
+        </header>
+      </div>
     );
   }
 }

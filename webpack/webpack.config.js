0cf6d3e8973ec40878a5756f6cc65529f8397f3d
kmsheng <kmsh3ng@gmail.com>
2016-05-17 13:02:06 +0800

    add webpack config

---
+++ webpack/webpack.config.js
@@ -0,0 +1,73 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var config = {
+  devtool: 'eval',
+  entry: {
+    javascript: [
+      'react-hot-loader/patch',
+      'webpack-dev-server/client?http://0.0.0.0:3000',
+      'webpack/hot/only-dev-server',
+      './src/index'
+    ],
+    html: './index.html'
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist'),
+    publicPath: 'http://localhost:3000/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    extensions: ['', '.js']
+  },
+  module: {
+    noParse: /\/levelup\//,
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        configFile: './.eslintrc',
+        exclude: /node_modules/
+      }
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        exclude: /node_modules/
+      },
+      { test: /\.css$/, loaders: 'style-loader!css-loader'},
+      {
+        test: /\.scss$/,
+        loader: 'style!css!sass?outputStyle=expanded'
+      },
+      {
+        test: /\.(ttf|eot|svg)$/,
+        loader: 'file-loader?name=[path][name].[ext]'
+      },
+      {
+        test: /\.woff2?$/,
+        loader: 'url-loader?name=[path][name].[ext]&limit=10000&minetype=application/font-woff'
+      },
+      {
+        test: /\.(png|jpg|cur)$/,
+        loader: 'url-loader?name=[path][name].[ext]&limit=8192'
+      },
+      {
+        test: /\.html$/,
+        loader: 'file-loader?name=[name].[ext]'
+      }
+    ]
+  }
+};
+
+if ('production' === process.env.NODE_ENV) {
+  config.entry.javascript = ['./src/index'];
+  delete config.output.publicPath;
+}
+
+module.exports = config;

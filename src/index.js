ad79e9f47346c1ca480e179e4a050eaac7fdc673
kmsheng <kmsh3ng@gmail.com>
2016-05-17 13:12:11 +0800

    use browserHistory

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ require('./index.scss');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HashHistory from 'react-router/lib/hashHistory';
+import {browserHistory} from 'react-router';
 import Root from './root';
 
-const history = new HashHistory();
-
-ReactDOM.render(<Root history={history} />, document.getElementById('root'));
+ReactDOM.render(<Root history={browserHistory} />, document.getElementById('root'));

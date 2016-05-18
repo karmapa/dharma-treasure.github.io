ee8e88fe469991cc953066b15e03f2d965f946b8
kmsheng <kmsh3ng@gmail.com>
2016-05-18 11:17:05 +0800

    add component Jumbotron

---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+export default class Jumbotron extends Component {
+
+  render() {
+    return (
+      <div className="jumbotron-wrapper">
+        <div className="jumbotron top">
+          <div className="bg" />
+        </div>
+        <div className="jumbotron bottom">
+          <div className="bg" />
+        </div>
+      </div>
+    );
+  }
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,2 @@
 export MenuButton from './MenuButton/MenuButton';
+export Jumbotron from './Jumbotron/Jumbotron';

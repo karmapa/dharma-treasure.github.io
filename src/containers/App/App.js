a86c33216385560b2cdf5f601b14c59d104b3cb5
kmsheng <kmsh3ng@gmail.com>
2016-05-18 11:19:26 +0800

    update container App

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,18 +1,30 @@
-import React, {Component} from 'react';
-import {MenuButton} from '../../components';
+import React, {Component, PropTypes} from 'react';
+import {MenuButton, Jumbotron} from '../../components';
+import {connect} from 'react-redux';
+import {setMenuStatus} from '../../modules/app';
 
+@connect(state => ({
+  isMenuOpened: state.app.get('isMenuOpened')
+}), {setMenuStatus})
 export default class App extends Component {
+
+  static propTypes = {
+    isMenuOpened: PropTypes.bool.isRequired,
+    setMenuStatus: PropTypes.func.isRequired
+  };
+
+  handleMenuButtonClick = () => this.props.setMenuStatus(! this.props.isMenuOpened);
 
   render() {
     return (
       <div className="home">
-        <div className="jumbotron"></div>
         <header>
           <div className="header">
             <h1>Dharma Treasure</h1>
-            <MenuButton />
+            <MenuButton opened={this.props.isMenuOpened} onClick={this.handleMenuButtonClick} />
           </div>
         </header>
+        <Jumbotron />
       </div>
     );
   }

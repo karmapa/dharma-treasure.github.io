8b1f72643fa304f2f8919e8c125d7422ea5dec46
kmsheng <kmsh3ng@gmail.com>
2016-05-18 14:35:21 +0800

    prevent bg zooming

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -2,14 +2,24 @@ import React, {Component} from 'react';
 
 export default class Jumbotron extends Component {
 
+  constructor(props) {
+    super(props);
+    this.wrapperWidth = 0;
+  }
+
+  componentDidMount() {
+    this.wrapperWidth = this.refs.jumbotronWrapper.offsetWidth;
+    this.forceUpdate();
+  }
+
   render() {
     return (
-      <div className="jumbotron-wrapper">
-        <div className="jumbotron top">
-          <div className="bg" />
+      <div ref="jumbotronWrapper" className="jumbotron-wrapper">
+        <div className="jumbotron top sliding">
+          <div className="bg" style={{width: this.wrapperWidth}} />
         </div>
-        <div className="jumbotron bottom">
-          <div className="bg" />
+        <div className="jumbotron bottom sliding">
+          <div className="bg" style={{width: this.wrapperWidth}} />
         </div>
       </div>
     );

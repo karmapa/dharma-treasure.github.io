f358efc0a0475fbf376002e760a035fa199fb0fa
kmsheng <kmsh3ng@gmail.com>
2016-05-18 16:22:28 +0800

    add function renderContent

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,7 +1,8 @@
 import React, {Component, PropTypes} from 'react';
-import {MenuButton, Jumbotron} from '../../components';
+import {Menu, MenuButton, Jumbotron} from '../../components';
 import {connect} from 'react-redux';
 import {setMenuStatus} from '../../modules/app';
+import classNames from 'classnames';
 
 @connect(state => ({
   isMenuOpened: state.app.get('isMenuOpened')
@@ -15,16 +16,27 @@ export default class App extends Component {
 
   handleMenuButtonClick = () => this.props.setMenuStatus(! this.props.isMenuOpened);
 
+  renderContent() {
+    if (this.props.isMenuOpened) {
+      return <Menu />;
+    }
+    return <Jumbotron />;
+  }
+
   render() {
+    const {isMenuOpened} = this.props;
+    const headingClassName = classNames({
+      hidden: isMenuOpened
+    });
     return (
       <div className="home">
         <header>
           <div className="header">
-            <h1>Dharma Treasure</h1>
-            <MenuButton opened={this.props.isMenuOpened} onClick={this.handleMenuButtonClick} />
+            <h1 className={headingClassName}>Dharma Treasure</h1>
+            <MenuButton opened={isMenuOpened} onClick={this.handleMenuButtonClick} />
           </div>
         </header>
-        <Jumbotron />
+        {this.renderContent()}
       </div>
     );
   }

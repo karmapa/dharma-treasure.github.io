2842be2dff457f8438f78edb5dfc46a64da089c6
kmsheng <kmsh3ng@gmail.com>
2016-05-18 11:18:02 +0800

    update module app

--- src/modules/app.js
+++ src/modules/app.js
@@ -1,7 +1,19 @@
-const defaultState = {};
-const actionsMap = {};
+import Immutable from 'immutable';
+import {createReducer} from 'redux-immutablejs';
 
-export default function reducer(state = defaultState, action) {
-  const reduceFn = actionsMap[action.type];
-  return reduceFn ? reduceFn(state, action) : state;
+const SET_MENU_STATUS = 'darma-treasure/app/SET_MENU_STATUS';
+
+const initialState = Immutable.Map({
+  isMenuOpened: false
+});
+
+export default createReducer(initialState, {
+  [SET_MENU_STATUS]: (state, action) => state.set('isMenuOpened', action.isMenuOpened)
+});
+
+export function setMenuStatus(isMenuOpened) {
+  return {
+    type: SET_MENU_STATUS,
+    isMenuOpened
+  };
 }
